1de9b964181235d1f1985e9ed4491503892111a1
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2022-10-14 10:07:11 +0100

    Add files via upload
    update changes and save the latest changes

---
+++ java 2.js
@@ -0,0 +1,8 @@
+<script>
+function MY_First_Function() {
+var String = "Sing with me, I'm Irish!";
+var result = String.fontcolor("green");
+document.getElementById("Irish").innerHTML =
+result;
+} 
+</script>
